7619be0ec9cf785c84819825ec9da35fef215623
DiffickMenLogo <gh.sdfs@mail.ru>
2023-02-03 07:53:23 +0300

    feat: make .env with port

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
